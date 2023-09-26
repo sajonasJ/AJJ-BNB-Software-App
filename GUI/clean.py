abec55b77eefed5e115773f61c171967e0f97d19
Jonas Sajonas <jonas.sajonas@gmail.com>
2023-09-26 16:02:19 +1000

    added clean.py, updated some indents

---
+++ GUI/clean.py
@@ -0,0 +1,35 @@
+# List of keywords for cleanliness
+cleanlinesskeywords = [
+    'dirty',
+    'clean',
+    'cleanliness',
+    'disgusting',
+    'disgust',
+    'mold',
+    'neat',
+    'filthy',
+    'fresh',
+    'spotless',
+    'dust',
+    'germ',
+    'immaculate',
+    'tidy',
+    'hygiene',
+    'hygenic',
+    'gleaming'
+    'unsoiled',
+    'sanitary',
+    'contaminated'
+    'mop',
+    'vacuum'
+    'decontaminate',
+    'orderly',
+    'rinse',
+    'cob',
+    'cobwebs',
+    'rodents',
+    'rodent',
+    'vermin',
+    'mice',
+    'mouse'
+]
